a08d8fc9b34d74caa3094215aa2df3dcfa2e7604
Francisco <fllaryora@gmail.com>
2019-11-16 19:31:42 -0300

    Se agrega ultima dependencia de Dagger 2

--- app/src/main/java/com/example/roompractice/presentation/MainActivity.kt
+++ app/src/main/java/com/example/roompractice/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.roompractice
+package com.example.roompractice.presentation
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import com.example.roompractice.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 

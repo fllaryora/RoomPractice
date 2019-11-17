69773af3172239e3ba50fdb943a57abbaccc537f
Francisco <fllaryora@gmail.com>
2019-11-17 20:15:03 -0300

    FL : Agrego el componente global appcomponent, le doy un modulo de esa forma puede compilar,implemento el metodo de la clase baseapplication

---
+++ app/src/main/java/com/example/roompractice/di/AppComponent.kt
@@ -0,0 +1,55 @@
+package com.example.roompractice.di
+
+import android.app.Application
+import com.example.roompractice.presentation.base.BaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import javax.inject.Singleton
+
+/**
+ * Component is a graph. We build a component. Component will provide injected instances by using modules.
+ * Extends appcomponent with [AndroidInjector] to avoid old way of injection application
+ *
+ * <code>
+ *     fun inject(application: BaseApplication)
+ * </code>
+ *
+ * AppComponent is act as a server whereas, [BaseApplication] act as a client.
+ * Dagger interaction is like client-server interaction
+ *
+ * Anotated with [Singleton] Scope to tell dagger to keep it in the memory while application exists
+ * and destroy it when application destroy
+ */
+
+@Singleton
+@Component (
+    modules = [
+        AndroidInjectionModule::class
+    ]
+)
+/**
+ * There is a relationship between AppComponent (as server) and BaseApplication (as client).
+ * This is the difference with the old way to do dagger.
+ */
+interface AppComponent : AndroidInjector<BaseApplication> {
+
+    /**
+     Overrrides the bilder
+     */
+    @Component.Builder
+    interface Builder {
+        /**
+         * [BindsInstance] annotation is used for, if you want to bind particular object or instance
+         * of an object through the time of component construction
+         */
+        @BindsInstance
+        fun application(application: Application) : Builder
+
+        /**
+         * Mandatory Step inside @Component.Builder
+         */
+        fun build() : AppComponent
+    }
+}
--- app/src/main/java/com/example/roompractice/presentation/base/BaseApplication.kt
+++ app/src/main/java/com/example/roompractice/presentation/base/BaseApplication.kt
@@ -1,5 +1,6 @@
 package com.example.roompractice.presentation.base
 
+import com.example.roompractice.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
@@ -13,6 +14,6 @@ import dagger.android.DaggerApplication
 
 class BaseApplication: DaggerApplication() {
     override fun applicationInjector(): AndroidInjector <out DaggerApplication>? {
-        return null
+        return DaggerAppComponent.builder().application(this).build()
     }
 }

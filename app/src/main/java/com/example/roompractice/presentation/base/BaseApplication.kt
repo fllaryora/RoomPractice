74f69058790b0d3212083fb52c94ef69f845e2ae
Francisco <fllaryora@gmail.com>
2019-11-17 19:21:38 -0300

    agrego el base application de dagger falta implementar el metodo injector de la clase

--- app/src/main/java/com/example/roompractice/presentation/base/BaseApplication.kt
+++ app/src/main/java/com/example/roompractice/presentation/base/BaseApplication.kt
@@ -3,6 +3,13 @@ package com.example.roompractice.presentation.base
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
+/**
+ * The Base Application which will live entire lifecycle of an application.
+ *
+ * I want to make our AppComponent live in the entire lifecycler of an
+ * application so we can instantiate it on applicationInjection
+ * overrided method which is comming from [DaggerApplication]
+ */
 
 class BaseApplication: DaggerApplication() {
     override fun applicationInjector(): AndroidInjector <out DaggerApplication>? {

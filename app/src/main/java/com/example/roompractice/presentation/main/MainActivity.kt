827c06a5a7e3dc169e4135a0d693c9f898464a1e
Francisco <fllaryora@gmail.com>
2019-11-18 19:36:21 -0300

    se implementa camino de deslogueo

--- app/src/main/java/com/example/roompractice/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/roompractice/presentation/main/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.roompractice.presentation.main
 
 import android.os.Bundle
-//import android.view.Menu
-//import android.view.MenuItem
+import android.view.Menu
+import android.view.MenuItem
 import com.example.roompractice.R
 import com.example.roompractice.presentation.base.BaseActivity
 
@@ -14,6 +14,22 @@ class MainActivity : BaseActivity() {
         //testFragment()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.logout -> {
+                sessionManager.logout()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
     /**
      * Test the fragment to inflate on [MainActivity]
      */
@@ -21,21 +37,5 @@ class MainActivity : BaseActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_container, ProfileFragment())
             .commit()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when(item?.itemId) {
-            R.id.logout -> {
-                sessionManager.logout();
-                return true
-            }
-        }
-
-        return super.onOptionsItemSelected(item)
     }*/
 }

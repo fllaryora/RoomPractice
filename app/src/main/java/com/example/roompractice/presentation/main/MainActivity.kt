4f7edd84bf33b2e1759ab6244b768cb0b2b7d4cf
Francisco <fllaryora@gmail.com>
2019-11-23 21:07:48 -0300

    controlling the backstack with navigation controller

--- app/src/main/java/com/example/roompractice/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/roompractice/presentation/main/MainActivity.kt
@@ -3,18 +3,25 @@ package com.example.roompractice.presentation.main
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.roompractice.R
 import com.example.roompractice.presentation.base.BaseActivity
-import com.example.roompractice.presentation.main.post.PostsFragment
-import com.example.roompractice.presentation.main.profile.ProfileFragment
+import com.example.roompractice.utils.Constants
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private val navController by lazy { findNavController(nav_host_fragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        testFragment()
+        initNavigation()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -23,26 +30,81 @@ class MainActivity : BaseActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when(item?.itemId) {
+        return when(item?.itemId) {
             R.id.logout -> {
                 sessionManager.logout()
-                return true
+                return Constants.CLICK_IS_CONSUMED
+            }
+
+            android.R.id.home -> {
+                if(drawer_layout.isDrawerOpen(GravityCompat.START)) {
+                    drawer_layout.closeDrawer(GravityCompat.START)
+                    return Constants.CLICK_IS_CONSUMED
+                } else {
+                    return Constants.CLICK_IS_NOT_CONSUMED
+                }
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
             }
         }
-
-        return super.onOptionsItemSelected(item)
     }
 
     /**
-     * Test the fragment to inflate on [MainActivity]
+     *  to inflate on [MainActivity]
      */
-    private fun testFragment() {
-       /* supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, ProfileFragment())
-            .commit()*/
+    private fun initNavigation() {
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, PostsFragment())
-            .commit()
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+        NavigationUI.setupWithNavController(nav_view, navController)
+        /**
+         * para que consuma los clicks locales en onNavigationItemSelected
+         */
+        nav_view.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.nav_profile -> {
+                    /**
+                     * Conditional Navigation:
+                     * Pop up to a given destination before navigating.
+                     * This pops all non-matching destinations from the back stack
+                     * until this destination is found.
+                     */
+                    val popUpTo = NavOptions.Builder().setPopUpTo(
+                        R.id.main,
+                        true
+                    ).build()
+                    navController.navigate(R.id.profileScreen, null, popUpTo)
+                Constants.CLICK_IS_CONSUMED
+            }
+            R.id.nav_posts -> {
+                if(isValidDestination(R.id.postScreen)) {
+                    navController.navigate(R.id.postScreen)
+                }
+            }
+            else -> {
+            }
+        }
+        item.isChecked = true
+        /**
+         * lo llevamos de vuelta al START si no seleccionamos nada al cerrar.
+         */
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return Constants.CLICK_IS_CONSUMED
+    }
+
+    /**
+     * Asumiendo que el up button el = pero con 3 barras.
+     * Maneja el up button delegando el comportamiento
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,drawer_layout)
+    }
+
+    private fun isValidDestination(destination:Int):Boolean {
+        return destination != navController.currentDestination?.id
     }
 }
--- app/src/main/java/com/example/roompractice/utils/Constants.kt
+++ app/src/main/java/com/example/roompractice/utils/Constants.kt
@@ -2,4 +2,6 @@ package com.example.roompractice.utils
 
 object Constants {
 
+    const val CLICK_IS_CONSUMED = true
+    const val CLICK_IS_NOT_CONSUMED = false
 }
